3fab856cbaad2681484207e3d23a3e822caa8c61
alan victor <79014674+alanzin1@users.noreply.github.com>
2022-12-13 21:44:47 -0300

    Update TR3_536784.c

--- TR3_536784.c
+++ TR3_536784.c
@@ -5,7 +5,9 @@
 //matricula: 536784
 
 int main(){
-\\criação das variaveis
+    
+//criação das variaveis e vetor
+    
     unsigned int z[200];
     int mem;
     int saida;
@@ -14,18 +16,27 @@ int main(){
     int pot;
     int i;
     
+//laço responsavel por zerar o vetor para usarmos o metodo bit a bit
+    
     for(i = 199; i >= 0; i--){
         z[i] = 0;
         }
-
+    
+//laço principal
+    
     while(1){
+        
+//parte responsavel por receber a entrada do usuario e usa-la para calcular alguns valores padroes usados futuramente no programa
+        
         printf("insira um numero: ");
         scanf("%d",&x);
         i = 0;
         pot = 1;
         mem = x/32;
         a = mem;
-
+        
+//condiçao de continuidade do programa
+        
         if(x != -1){
 
             a = x - (a*32);
@@ -41,7 +52,9 @@ int main(){
         }
 
         else{
-
+            
+//laço responsavel de printar para o usuario os dados de entrada já processados
+            
             for(saida = 0; saida < 200; saida++){
                 for(i = 0; i < 32; i++) {
                     mem = z[saida] >> i;
